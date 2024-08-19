b741a618d6abe60c0c640d5ae5305b431549d660
Dion Hobdy <devdionhobdy@gmail.com>
2024-08-19 04:47:40 -0700

    Add files via upload

--- Batch Files/SSH/ssh-admin.bat
+++ Batch Files/SSH/ssh-admin.bat
@@ -3,41 +3,40 @@ CLS
 setlocal enabledelayedexpansion
 
 echo.
-echo ███████╗███████╗██╗  ██╗       █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗
-echo ██╔════╝██╔════╝██║  ██║      ██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║
-echo ███████╗███████╗███████║█████╗███████║██║  ██║██╔████╔██║██║██╔██╗ ██║
-echo ╚════██║╚════██║██╔══██║╚════╝██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║
-echo ███████║███████║██║  ██║      ██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║
-echo ╚══════╝╚══════╝╚═╝  ╚═╝      ╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝
+echo    _____ _____ _____     _____ ____  _____ _____ ______
+echo   /   __/   __/  /  /___/  _  /    \/     /     /   / /
+echo  /__   /__   /     /___/     /  /  / / / /-   -/ / / /
+echo /_____/_____/__/__/   /__/__/____ /_/_/_/_____/_/___/
 echo.
 
-rem dateStr variable used for logging whenever the script is launched
-for /f "tokens=1-3 delims=/ " %%a in ('echo %date%') do (
-    set 'year=%%c'
-    set 'month=%%a'
-    set 'day=%%b'
-
-)
-set dateStr="%day%-%month%-%year%"
-
 rem create Script_Logs folder to store logging information
-if not exist %Script_Logs%\ ( mkdir %Script_Logs% )
+if not exist script_logs\ ( mkdir script_logs )
 rem CD into Script_Logs folder to ensure that the logs are being saved into the correct folder
-cd %Script_Logs%
+cd script_logs
 
 rem create ssh-admin-use.txt to log when the script is ran
 if not exist ssh-admin-use.txt ( type nul > ssh-admin-use.txt )
-rem write dateStr variable to ssh-admin-use.txt
-%dateStr% >ssh-admin-use.txt
 
 rem ping the host ip to verify that you can connect
+ping HOST_IP_HERE
 ping HOST_IP_HERE | find "TTL=" >nul
 rem notify user of ping result before reaching host
 if errorlevel 1 (
-    echo {0C}error: host is not reachable. please try again.{#}{\n}
+    echo.
+    echo error: host is not reachable. please try again.
+    rem write dateStr variable to ssh-admin-use.txt
+    echo %date% - error: host is not reachable - %time% >>ssh-admin-use.txt
+    echo.
+    pause
 ) else (
-    echo {02}success: host is reachable. proceed to ssh.{#}{\n}
+    echo.
+    echo success: host is reachable. proceed to ssh.
+    rem write dateStr variable to ssh-admin-use.txt
+    echo %date% - success: host is reachable - %time% >>ssh-admin-use.txt
+    echo.
+    pause
 )
 
 rem ssh into host
+echo.
 ssh admin@HOST_IP_HERE
