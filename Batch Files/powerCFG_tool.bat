7f0ff62d3eff711679c7b80a7ba5d9232471c255
Dion Hobdy <devdionhobdy@gmail.com>
2024-08-19 03:05:30 -0700

    Update powerCFG_tool.bat

--- Batch Files/powerCFG_tool.bat
+++ Batch Files/powerCFG_tool.bat
@@ -30,7 +30,7 @@ goto exit
 
 rem dir_ function. Create folder for host computer if not existing. CD to host computer folder.
 :dir_
-cd C:\Users\dionh\OneDrive\Desktop\Dion\SYS\sys_logs\windows\powercfg
+cd INSERT_FOLDER_PATH_HERE
 echo             _______&& color 0E
 echo      ______/  ____/_____    
 echo     /     /  /         /
